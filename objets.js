c022f49aefd07c6a5d374d49f31b05d1bcbdc4e0
yoann53 <yoann.gauchard@gmail.com>
2017-02-14 17:05:23 +0100

    objet avec méthode

--- objets.js
+++ objets.js
@@ -26,14 +26,18 @@ maFonction();
 
 
 //Fonction avec return
-var x = 2;
-var y = 5;
+var x = 2; //variable globale
+var y = 5; //variable globale
 function maDeuxiemeFonction() {
+	var z = 4; //variable locale à la fonction (on ne s'en sert que dedans);
+	console.log("val de z au sein de son scope : " + z);
 	return x * y;
 }
 
 var resultat = maDeuxiemeFonction();
 console.log(resultat);
+console.log("val de z en dehors de son scope : " + z); // ne peut pas s'afficher
+// car la portée de la variable z ne vas au-delà de la fonction où elle est déclarée.
 
 
 //Fonction avec arguments
@@ -44,9 +48,17 @@ function maTroisiemeFonction($arg1, $arg2) {
 var resultat2 = maTroisiemeFonction(10, 20);
 console.log(resultat2);
 
+//Déclaration d'un objet avec une méthode adresse
+var monObj2 = {
+	nom: "HAMELIN",
+	prenom: "Paul",
+	age: 18,
+	adresse = function() {
+		return "44 rue du paradis";
+	}
+}
 
-
-
+console.log(monObj2.adresse());
 
 
 
